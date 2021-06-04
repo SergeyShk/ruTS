dcbbcd0d06797f8dbfd74cbac31da52c62f6b6ef
Shkarin Sergey <kouki.sergey@gmail.com>
2021-06-04 15:27:51 +0300

    Разработаны тесты для визуализации Литературная Дактилоскопия

---
+++ tests/visualizers/test_fingerprinting.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import pytest
+from matplotlib.figure import Figure
+
+from ruts.diversity_stats import calc_simpson_index
+from ruts.visualizers import fingerprinting
+
+
+@pytest.fixture(scope="module")
+def texts():
+    return [["мама", "мыла", "раму"], ["шла", "Саша", "по", "Шоссе"]]
+
+
+def test_fingerprinting_type_error():
+    with pytest.raises(TypeError):
+        fingerprinting(1)
+
+
+def test_fingerprinting(texts):
+    plt.cla()
+    plot = fingerprinting(texts, x_size=600, y_size=500)
+    assert type(plot) == Figure
+    assert len(plot.axes) == 2
+    assert plot.axes[0].title.get_text() == "Литературная дактилоскопия"
+    assert plot.axes[0].title.get_position() == (0.5, 1.0)
+    assert plot.axes[1]._label == "<colorbar>"
+    assert plot.axes[0].get_xlim() == (-600.0, 600.0)
+    assert plot.axes[0].get_ylim() == (-500.0, 500.0)
+
+
+def test_fingerprinting_is_return(texts):
+    plt.cla()
+    plot = fingerprinting(texts, is_return=False)
+    assert plot is None
+
+
+def test_fingerprinting_metric(texts):
+    plt.cla()
+    plot = fingerprinting(texts, metric=calc_simpson_index)
+    assert type(plot) == Figure
--- tests/visualizers/test_zipf.py
+++ tests/visualizers/test_zipf.py
@@ -1,5 +1,6 @@
 from typing import Counter
 
+import matplotlib.pyplot as plt
 import pytest
 from matplotlib.lines import Line2D
 
@@ -12,6 +13,7 @@ def tokens():
 
 
 def test_zipf_theory():
+    plt.cla()
     plot = zipf_theory(10, 5, 1.0)[0]
     assert type(plot) == Line2D
     assert plot._label == "Теоретический закон"
@@ -25,6 +27,7 @@ def test_zipf_type_error():
 
 
 def test_zipf(tokens):
+    plt.cla()
     plot = zipf(tokens)
     assert type(plot) == Line2D
     assert plot._label == "Экспериментальный закон"
@@ -33,29 +36,34 @@ def test_zipf(tokens):
 
 
 def test_zipf_num_words(tokens):
+    plt.cla()
     plot = zipf(tokens, num_words=2)
     assert len(plot.get_data()[0]) == 2
 
 
 def test_zipf_log(tokens):
+    plt.cla()
     plot = zipf(tokens, log=False)
     assert plot.axes.get_xscale() == "linear"
     assert plot.axes.get_yscale() == "linear"
 
 
 def test_zipf_num_labels(tokens):
+    plt.cla()
     plot = zipf(tokens, num_labels=1)
     assert plot.axes.get_children()[1].get_text() == " а"
     assert plot.axes.get_children()[1].get_position() == (1, 100)
 
 
 def test_zipf_show_theory(tokens):
+    plt.cla()
     plot = zipf(tokens, show_theory=True)[0]
     assert plot.axes.get_children()[0]._label == "Экспериментальный закон"
     assert plot.axes.get_children()[1]._label == "Теоретический закон"
 
 
 def test_zipf_with_theory(tokens):
+    plt.cla()
     plot = zipf(tokens, show_theory=True)
     assert len(plot) == 1
     assert all(type(p) == Line2D for p in plot)
